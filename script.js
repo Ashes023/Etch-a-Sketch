cadcdf854867673dde31576b39cad60cc0f7a971
Ashes023! <ashaharyunuskhan@gmail.com>
2024-03-13 02:29:26 +0530

    Structure Done

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let grid=document.getElementById('grid');
-for(let x=0;x<16;x++){
-    let colums=document.createElement('div');
-    colums.setAttribute('class','hori');
-    grid.appendChild(colums);
-    for(let y=0;y<16;y++){
-        let rows=document.createElement('div');
-        rows.setAttribute('class','vert');
-        colums.appendChild(rows);
-    }
+let grid = document.getElementById("grid");
+for (let x = 0; x < 16; x++) {
+  let colums = document.createElement("div");
+  colums.classList.add("hori");
+  grid.appendChild(colums);
+  for (let y = 0; y < 16; y++) {
+    let rows = document.createElement("div");
+    rows.classList.add("vert");
+    colums.appendChild(rows);
+    rows.addEventListener("mouseover", () => {
+        rows.classList.add("hold");
+      });
+  }
 }
